35624340f3d6d43fbdf12d571d262c096694bb16
NikolayWTF <kolan.gregorin01@gmail.com>
2021-10-16 22:08:29 +0300

    Увеличил границы экрана и сделал корректный расчёт соседей для областей, граничащих с экраном

--- src/Main.java
+++ src/Main.java
@@ -38,7 +38,7 @@ public class Main
 
     public static void Print()
     {
-        System.out.println("Введите через пробел номера квадратов, в которых изначально будут находится особи (от 1 до 522) ");
+        System.out.println("Введите через пробел номера квадратов, в которых изначально будут находится особи (от 1 до 2088) ");
     }
 
 }
@@ -68,14 +68,14 @@ class net extends JComponent {
             repaint();
         }
     };
-    Timer timer = new Timer(1500,timerListener);
+    Timer timer = new Timer(500,timerListener);
 
     public void paint(Graphics g)
     {
         timer.stop();
-        for (int j = 0; j < 18; j ++) {
-            for (int i = 0; i < 29; i++) {
-                g.drawRect(i*50, j*50, 50, 50);
+        for (int j = 0; j < 36; j ++) {
+            for (int i = 0; i < 58; i++) {
+                g.drawRect(i*25, j*25, 25, 25);
             }
         }
         Graphics2D g2 = (Graphics2D) g;
@@ -83,9 +83,9 @@ class net extends JComponent {
         for (int i = 0; i < Array.live.size(); i ++)
         {
             a = Array.live.get(i);
-            b = (a-1) % 29;
-            c = Integer.valueOf((int) Math.floor((a - 1) / 29));
-            Square square = new Square(b * 50, c * 50);
+            b = (a-1) % 58;
+            c = Integer.valueOf((int) Math.floor((a - 1) / 58));
+            Square square = new Square(b * 25, c * 25);
             square.Draw(g2);
         }
         int j = 0, neighbour, ind;
@@ -93,20 +93,20 @@ class net extends JComponent {
 
             ArrayList<Integer> die = new ArrayList<>();
             ArrayList<Integer> birth = new ArrayList<>();
-            for (int i = 0; i < 522; i++)
+            for (int i = 0; i < 2088; i++)
             {
                 neighbour = 0;
                 a = i + 1;
                 b = 1; // Ещё не родился
                 if (Array.IN(a)) b = 0; // Уже родился
-                if (Array.IN(a+1)) neighbour += 1;
-                if (Array.IN(a-1)) neighbour += 1;
-                if (Array.IN(a+28)) neighbour += 1;
-                if (Array.IN(a+29)) neighbour += 1;
-                if (Array.IN(a+30)) neighbour += 1;
-                if (Array.IN(a-28)) neighbour += 1;
-                if (Array.IN(a-29)) neighbour += 1;
-                if (Array.IN(a-30)) neighbour += 1;
+                if (a%58 != 0) if (Array.IN(a+1)) neighbour += 1;
+                if (a%58 != 1) if (Array.IN(a-1)) neighbour += 1;
+                if(Math.floor(a/36) != 35) if (Array.IN(a+58)) neighbour += 1;
+                if(Math.floor(a/36) != 35 && a%58 != 0)  if (Array.IN(a+59)) neighbour += 1;
+                if(Math.floor(a/36) != 35 && a%58 != 1)  if (Array.IN(a+57)) neighbour += 1;
+                if(Math.floor(a/36) != 0) if (Array.IN(a-58)) neighbour += 1;
+                if(Math.floor(a/36) != 0 && a % 58 != 0)  if (Array.IN(a-57)) neighbour += 1;
+                if(Math.floor(a/36) != 0 && a%58 != 1)  if (Array.IN(a-59)) neighbour += 1;
 
                 if (b == 1)
                 {
@@ -145,16 +145,14 @@ class Square {
 
     public void Draw(Graphics2D g2){
         g2.setColor(Color.green);
-        g2.fillRect(x, y, 50, 50);
+        g2.fillRect(x, y, 25, 25);
     }
 
 }
 
+//283 284 285 286 287 341 342 343 344 345
+// 720 721 722 778 779 780 836 837 838 724 725 726 782 783 784 840 841 842
+// 700 701 702 703 758 759 760 761 816 817 818 819 874 875 876 877 704 762 820 878
+// 720 721 722 723 778 779 780 781 836 837 838 839 904 905 906 907 734 792 850 908
+// 700 701 702 703 758 759 760 761 816 817 818 819 874 875 876 877 704 762 820 878 720 721 722 723 778 779 780 781 836 837 838 839 904 905 906 907 734 792 850 908
 
-
-// Понятная закономерность с одним квадратом
-// 183 184 185 212 213 214 241 242 243
-// Два квадрата и более замысловатые изображения
-// 189 190 191 218 219 220 247 248 249 183 184 185 212 213 214 241 242 243
-// Один квадрат - усточивое положение, изменений нет
-// 183 184 212 213
